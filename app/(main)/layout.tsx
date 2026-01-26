e13502c96b3a936e9d512c3f7957fb68ae2d26a4
shivamkarn <shivamkarn767@gmail.com>
2026-01-26 18:13:57 +0545

    Update MainLayout padding for improved spacing and visual consistency

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ChildrenType } from "../(auth)/layout";
 
 const MainLayout = ({ children }: ChildrenType) => {
-  return <div className="container mx-auto my-8 sm:my-32 px-2 sm:px-4 max-w-full overflow-x-hidden">{children}</div>;
+  return <div className="container mx-auto pt-20 sm:pt-24 pb-8 sm:pb-32 px-2 sm:px-4 max-w-full overflow-x-hidden">{children}</div>;
 };
 
 export default MainLayout;
