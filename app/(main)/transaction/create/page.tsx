5e97e4c1310da7af2f8db876a39f6f7f2cded099
shivamkarn <shivamkarn767@gmail.com>
2026-02-01 12:05:39 +0545

    /transaction/create page basic setup

--- app/(main)/transaction/create/page.tsx
+++ app/(main)/transaction/create/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { getUserAccounts } from "@/actions/dashboard";
+import AddTransactionForm from "./_components/transaction-form";
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
 
-const AddTransactionPage = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
+      {/* <AddTransactionForm accounts={accounts} categories={defaultCategories} /> */}
+    </div>
+  );
 };
 
 export default AddTransactionPage;
