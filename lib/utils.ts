edc9162b6c050f24b281bca1185bc5c960d2542e
shivamkarn <shivamkarn767@gmail.com>
2026-02-03 19:53:57 +0545

    Add calculateNextRecurringDate utility function for date calculations

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ReccuringInterval } from "./generated/prisma/enums";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function calculateNextRecurringDate(
+  startDate: Date,
+  interval: ReccuringInterval,
+): Date {
+  const date = new Date(startDate);
+
+  switch (interval) {
+    case "DAILY":
+      date.setDate(date.getDate() + 1);
+      break;
+    case "WEEKLY":
+      date.setDate(date.getDate() + 7);
+      break;
+    case "MONTHLY":
+      date.setMonth(date.getMonth() + 1);
+      break;
+    case "YEARLY":
+      date.setFullYear(date.getFullYear() + 1);
+      break;
+  }
+
+  return date;
 }
