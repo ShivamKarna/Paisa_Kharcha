5e366f938aaba88a364c463923d7e140449edbb2
shivamkarn <shivamkarn767@gmail.com>
2026-02-03 20:43:33 +0545

    Update header to conditionally display "Update Transaction" or "Add Transaction"

--- app/(main)/transaction/create/page.tsx
+++ app/(main)/transaction/create/page.tsx
@@ -21,7 +21,9 @@ const AddTransactionPage = async ({
 
   return (
     <div className="max-w-3xl mx-auto px-5">
-      <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
+      <h1 className="text-5xl gradient-title mb-8">
+        {editId ? "Update Transaction" : "Add Transaction"}
+      </h1>
       <AddTransactionForm
         accounts={accounts}
         categories={defaultCategories}
