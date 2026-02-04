fa3fb4825641b85a224a0d65c3f30615dd5cab17
shivamkarn <shivamkarn767@gmail.com>
2026-02-04 20:57:55 +0545

    Add fallback redirect and sign-up URL to SignIn component

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,10 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <SignIn />
+      <SignIn 
+        fallbackRedirectUrl="/dashboard"
+        signUpUrl="/sign-up"
+      />
     </div>
   );
 };
