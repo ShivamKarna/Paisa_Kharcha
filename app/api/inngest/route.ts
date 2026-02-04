ddd58fe3a15936e89063a81c3b6bc59a7c603510
shivamkarn <shivamkarn767@gmail.com>
2026-02-04 19:49:48 +0545

    Add generateMonthlyReports function to API route for enhanced reporting capabilities

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import {
   checkBudgetAlert,
+  generateMonthlyReports,
   processRecurringTransaction,
   triggerRecurringTransactions,
 } from "@/lib/inngest/functions";
@@ -13,5 +14,6 @@ export const { GET, POST, PUT } = serve({
     checkBudgetAlert,
     triggerRecurringTransactions,
     processRecurringTransaction,
+    generateMonthlyReports,
   ],
 });
